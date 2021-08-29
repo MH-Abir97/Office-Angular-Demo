497707e9a19f8a2eabd9054cebd33e5d92f30e2c
MH-Abir97 <71643559+MH-Abir97@users.noreply.github.com>
2021-08-29 11:51:38 +0600

    Paginator

--- src/app/test/pagination-list/pagination-list.component.ts
+++ src/app/test/pagination-list/pagination-list.component.ts
@@ -15,11 +15,22 @@ export class PaginationListComponent implements OnInit {
   dataSource:any;
   displayedColumns: string[] = ['SL','InvoiceNo','CompanyName', 'InvoiceDate', 'SalesOrderNos','DocStatus'];
  
-  length = 500;
-  pageSize = 10;
-  pageIndex = 0;
-  pageSizeOptions = [5, 10, 25];
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator
+ }
+  // length = 500;
+  // pageSize = 10;
+  // pageIndex = 0;
+  // pageSizeOptions = [5, 10, 25];
   showFirstLastButtons = true;
+
+  filterValue='';
+  applyFilter(event:Event){
+    this.filterValue=(event.target as HTMLInputElement).value;
+    console.log(this.filterValue);
+
+    this.getNextData("10","0","10",this.filterValue,"0");
+  }
 
   handlePageEvent(event: PageEvent) {
     var length = event.length;
@@ -28,7 +39,9 @@ export class PaginationListComponent implements OnInit {
     var previousIndex = event.previousPageIndex;
     var previousSize = pageSize * pageIndex;
     var rows = '0';
-    this.getNextData(previousSize,(pageIndex).toString(), pageSize.toString(), rows);
+    var whereCluse=this.filterValue;
+    console.log("whereCluse",whereCluse)
+    this.getNextData(previousSize,(pageIndex).toString(), pageSize.toString(),whereCluse.toString(), rows);
   } 
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -40,36 +53,51 @@ export class PaginationListComponent implements OnInit {
 
   ngOnInit(): void {
    
-    this.GetPaged('0','5', '0');
+   // this.getNextData('10','0','10','0')
+   
+   
+    if(this.filterValue !=""){
+      this.GetPaged('0','100','', '0');
+    }else{
+      this.GetPaged('0','10','', '0');
+    }
   }
 
-  getNextData(currentSize, startRecordNo, rowPerPage, rows){
+  getNextData(currentSize, startRecordNo, rowPerPage,whereCluse, rows){
     var params = new HttpParams();
     params = params.set('startRecordNo', startRecordNo);
     params = params.set('rowPerPage', rowPerPage);
+    params = params.set('whereCluse', whereCluse);
     params = params.set('rows', rows);
     this._itemSs.expGetaPaged(params.toString()).subscribe(data=>{
-      //data.ListData.length = currentSize;
+      data.ListData.length = currentSize;
       data.ListData.push(...data.ListData);
       data.ListData.length = data.TotalRecord;
      this.dataSource = new MatTableDataSource<any[]>(data.ListData);
+    // this.paginator.firstPage();
+     this.dataSource._updateChangeSubscription();
      this.dataSource.paginator = this.paginator;
     })
     
   }
 
   
-  GetPaged(startRecordNo, rowPerPage, rows){
+  GetPaged(startRecordNo, rowPerPage,whereCluse, rows){
     var params = new HttpParams();
+    //var wc=whereCluse.toString();
     params = params.set('startRecordNo', startRecordNo);
     params = params.set('rowPerPage', rowPerPage);
+    params = params.set('whereCluse',whereCluse);
     params = params.set('rows', rows);
     this._itemSs.expGetaPaged(params.toString()).subscribe(data=>{
       // var dataList:any[]=[];
       // console.log('List Data', data.ListData)
      //  dataList=data.ListData;
-      
+     //data.ListData.length = currentSize;
+      data.ListData.push(...data.ListData);
+      data.ListData.length = data.TotalRecord;
      this.dataSource = new MatTableDataSource<any[]>(data.ListData);
+     //this.paginator.firstPage();
      this.dataSource.paginator = this.paginator;
       //  data.ListData.forEach(element => {
       //   var getPagedListObj:any={};
